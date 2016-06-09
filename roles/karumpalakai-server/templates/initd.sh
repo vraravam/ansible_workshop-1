3631df100ab0511a9bb752fc52f96a62f7eef0cd
Aruna Shanmugakumar <arunash@thoughtworks.com>
2016-06-09 08:23:06 +0530

    [Aruna] #0, Deploying karumpalakai server done

--- roles/karumpalakai-server/templates/initd.sh
+++ roles/karumpalakai-server/templates/initd.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 # chkconfig: 345 80 20
-# description: Karumpalakai
+# description: karumpalakai
 ### BEGIN INIT INFO
 # Provides: karumpalakai
-# Required-Start: $network postgresql-9.3
-# Required-Stop: $network postgresql-9.3
 # Default-Start: 3 4 5
 # Default-Stop: 0 1 2 6
-# Description: Starts the karumpalakai service
+# Description: Starts karumpalakai
 ### END INIT INFO
 
 APP_NAME=karumpalakai
+JAR_NAME=Karum_palakai-1.0-SNAPSHOT
 APP_PORT=8080
 
-SERVICE_DIR=/application/$APP_NAME
+SERVICE_DIR=/application/
 SERVICE_BASE_URL=http://localhost:$APP_PORT
-CUR_USER=`whoami`
-
-LOGS=$SERVICE_DIR/karumpalakai.log
 
 # Lsb functions to perform the operations.
 #. /lib/lsb/init-functions
@@ -30,21 +26,19 @@ log_failure_msg() {
 }
 
 check_proc() {
-  pgrep -u karumpalakai -f $SERVICE_DIR/$APP_NAME.jar >/dev/null
+  pgrep -u karumpalakai -f $SERVICE_DIR/$JAR_NAME.jar >/dev/null
 }
 
-start_service() {
-  if [ "${CUR_USER}" != "root" ] && [ "${CUR_USER}" != "karumpalakai" ]; then
-    log_failure_msg "karumpalakai can only be started as 'root' or karumpalakai user."
-    exit 4
-  fi
 
+
+start_service() {
   check_proc
   if [ $? -eq 0 ]; then
     log_success_msg "karumpalakai already running."
     exit 0
   fi
 
+    nohup /bin/su - karumpalakai -c "java  -jar $SERVICE_DIR/$JAR_NAME.jar  -Dhttp.port=$APP_PORT --server.port=$APP_PORT" 2> /application/error.log > /application/karumpalakai.log &
   sleep 1
   check_proc
   if [ $? -eq 0 ]; then
